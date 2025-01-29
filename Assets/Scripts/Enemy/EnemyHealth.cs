f54fddaeeae4127ba7513a2726d336f273594514
turanak111 <turanak111@gmail.com>
2025-01-29 17:01:25 +0300

    added health bar for enemies

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -4,18 +4,19 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 public class EnemyHealth : MonoBehaviour
 {
     public float enemyHealth;
+
+    [SerializeField] private Image healthBar;
     void Start()
     {
-        if(gameObject.GetComponent<MeleeEnemyMove>()!=null) //Düşman melee ise
+        if (gameObject.GetComponent<MeleeEnemyMove>() != null) //Düşman melee ise ranged ise rangedstats eklenmeli
         {
             enemyHealth = MeleeEnemyStats.meleeEnemyHealth;
         }
-            //Ranged ise RangedEnemyStats...
     }
-
 
 
 
@@ -24,6 +25,7 @@ public class EnemyHealth : MonoBehaviour
     public void TakeDamageEnemy(float amount)
     {
         enemyHealth -= amount;
+        healthBar.fillAmount = 1 - (enemyHealth / 100);
         Debug.Log("enemy health is: " + enemyHealth);
         if (enemyHealth <= 0)
         {
