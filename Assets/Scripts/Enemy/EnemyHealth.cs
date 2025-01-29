17afb165482585f49f2cc8ccdf846317aa439a14
turanak111 <turanak111@gmail.com>
2025-01-29 15:46:16 +0300

    updates enemies: now they are moving with moving animation and they are changing their directions to the enemy with scale manipulation

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -7,37 +7,14 @@ using UnityEngine.Events;
 public class EnemyHealth : MonoBehaviour
 {
     public float enemyHealth = 100;
-    public SPUM_Prefabs _prefabs;
-    private PlayerState _currentState;
-    public Dictionary<PlayerState, int> IndexPair = new();
     void Start()
     {
-          if (_prefabs == null)
-        {
-            _prefabs = transform.GetChild(0).GetComponent<SPUM_Prefabs>();
-            if (!_prefabs.allListsHaveItemsExist())
-            {
-                _prefabs.PopulateAnimationLists();
-            }
-        }
-
-        _prefabs.OverrideControllerInit();
-
-        foreach (PlayerState state in Enum.GetValues(typeof(PlayerState)))
-        {
-            IndexPair[state] = 0;
-        }
     }
 
-    public void PlayStateAnimation(PlayerState state)
-    {
-        _prefabs.PlayAnimation(state, IndexPair[state]);
-    }
 
-    void Update()
-    {
-        PlayStateAnimation(_currentState);
-    }
+
+
+
 
     public void TakeDamageEnemy(float amount)
     {
@@ -51,7 +28,7 @@ public class EnemyHealth : MonoBehaviour
     private void EnemyDie()
     {
 
-         Destroy(gameObject);
+        Destroy(gameObject);
 
     }
 }
--- Assets/Scripts/Enemy/MeleeEnemyMove.cs
+++ Assets/Scripts/Enemy/MeleeEnemyMove.cs
@@ -1,10 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MeleeEnemyMove : MonoBehaviour
 {
-    
+    public Dictionary<PlayerState, int> IndexPair = new();
+     public SPUM_Prefabs _prefabs;
+     public GameObject unitRoot;
     public PlayerMovement playerMovement;
     Vector3 target_position;
     Vector3 enemy_position;
@@ -12,7 +17,30 @@ public class MeleeEnemyMove : MonoBehaviour
     public float meleeEnemySpeed;
 
     void Start()
+    
     {
+        if(unitRoot == null)
+        {
+            unitRoot = this.transform.GetChild(0).gameObject;
+        }
+         if (_prefabs == null)
+        {
+            _prefabs = GetComponent<SPUM_Prefabs>();
+            if (!_prefabs.allListsHaveItemsExist())
+            {
+                _prefabs.PopulateAnimationLists();
+            }
+        }
+
+        _prefabs.OverrideControllerInit();
+
+        foreach (PlayerState state in Enum.GetValues(typeof(PlayerState)))
+        {
+            IndexPair[state] = 0;
+        }
+        
+    
+
         if (playerMovement == null)
         {
             playerMovement = FindObjectOfType<PlayerMovement>();
@@ -22,7 +50,7 @@ public class MeleeEnemyMove : MonoBehaviour
     void Update()
     {
      
-
+        _prefabs.PlayAnimation(PlayerState.MOVE, IndexPair[PlayerState.MOVE]);
         target_position = playerMovement.transform.position; 
         enemy_position = transform.position; 
        
@@ -38,5 +66,10 @@ public class MeleeEnemyMove : MonoBehaviour
     {
         Vector3 direction = FindShortestPath();
         transform.position += direction * meleeEnemySpeed * Time.deltaTime; 
+        if(transform.position.x < playerMovement.transform.position.x)
+        unitRoot.transform.localScale = new Vector3(-1,1,1);
+        else if (transform.position.x > playerMovement.transform.position.x)
+        unitRoot.transform.localScale = new Vector3(1,1,1);
+
     }
 }
