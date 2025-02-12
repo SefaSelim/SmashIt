bc200ae4d904db10349793db6d392d0fbf052bbe
turanak111 <turanak111@gmail.com>
2025-02-12 23:36:43 +0300

    added ranged enemy, movement system and shoot system for ranged enemy

---
+++ Assets/Scripts/Enemy/Bullet.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float bulletspeed = 15f;
+    public float bulletdamage = 10f;
+     GameObject playerMovement;
+     Vector3 target_position;
+     Vector3 enemy_position;// Start is called before the first frame update
+    float timerForDestroy = 0;
+    public Vector3 direction;
+
+    void Start()
+    {
+        playerMovement = GameObject.Find("PlayerCharacter");
+         target_position = playerMovement.transform.position; 
+         enemy_position = transform.position; 
+        direction = FindShortestPath();
+    }
+
+     Vector3 FindShortestPath()
+    {
+        return (target_position - enemy_position).normalized;
+    }
+    void Update()
+    {
+        BulletMove();
+        timerForDestroy += Time.deltaTime;
+        if(timerForDestroy > 10)
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+    void BulletMove()
+    {   
+        target_position = playerMovement.transform.position; 
+        enemy_position = transform.position; 
+        
+        transform.position += direction * bulletspeed * Time.deltaTime; 
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            PlayerStats.PlayerHealth -= bulletdamage;
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/RangedEnemyMove.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class RangedEnemyMove : MonoBehaviour
+{
+    public Dictionary<PlayerState, int> IndexPair = new();
+     public SPUM_Prefabs _prefabs;
+     public GameObject unitRoot;
+    public PlayerMovement playerMovement;
+    Vector3 target_position;
+    Vector3 enemy_position;
+    public float shootCooldownTimer;
+    public GameObject bulletOfRangedEnemy;
+     bool isbulletshooted = false;
+    
+
+    void Start()
+    
+    {
+        if(unitRoot == null)
+        {
+            unitRoot = this.transform.GetChild(0).gameObject;
+        }
+         if (_prefabs == null)
+        {
+            _prefabs = GetComponent<SPUM_Prefabs>();
+            if (!_prefabs.allListsHaveItemsExist())
+            {
+                _prefabs.PopulateAnimationLists();
+            }
+        }
+
+        _prefabs.OverrideControllerInit();
+
+        foreach (PlayerState state in Enum.GetValues(typeof(PlayerState)))
+        {
+            IndexPair[state] = 0;
+        }
+        
+    
+
+        if (playerMovement == null)
+        {
+            playerMovement = FindObjectOfType<PlayerMovement>();
+        }
+    }
+
+    void Update()
+    {
+     
+        
+        target_position = playerMovement.transform.position; 
+        enemy_position = transform.position; 
+        if((Math.Abs(target_position.x - enemy_position.x) * Math.Abs(target_position.x - enemy_position.x
+         )) + (Math.Abs(target_position.y - enemy_position.y) * Math.Abs(target_position.y - enemy_position.y)) > 50f)
+         {
+             MoveRangedEnemy();
+         }
+         else{_prefabs.PlayAnimation(PlayerState.IDLE, IndexPair[PlayerState.IDLE]);}
+          if(transform.position.x < playerMovement.transform.position.x)
+        unitRoot.transform.localScale = new Vector3(-1,1,1);
+        else if (transform.position.x > playerMovement.transform.position.x)
+        unitRoot.transform.localScale = new Vector3(1,1,1);
+       
+       RangedEnemyShoot();
+    }
+
+    Vector3 FindShortestPath()
+    {
+        return (target_position - enemy_position).normalized;
+    }
+
+    void MoveRangedEnemy()
+    {
+        _prefabs.PlayAnimation(PlayerState.MOVE, IndexPair[PlayerState.MOVE]);
+        Vector3 direction = FindShortestPath();
+        transform.position += direction * RangedEnemyStats.rangedEnemySpeed * Time.deltaTime; 
+       
+
+    }
+    void RangedEnemyShoot()
+    {
+       
+         shootCooldownTimer += Time.deltaTime;
+         if(shootCooldownTimer > RangedEnemyStats.rangedEnemyCooldown && !isbulletshooted)
+         {
+            //Shoot
+            isbulletshooted = true;
+            Instantiate(bulletOfRangedEnemy,enemy_position,Quaternion.identity);
+            
+            shootCooldownTimer = 0;
+            isbulletshooted = false;
+         }
+    }
+}
